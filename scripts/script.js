6fd858fe7e694c0728d9d5e081b28fd6eb584caf
alexpryp <alexpryp@gmail.com>
2019-01-15 12:20:07 +0200

    Added historical content

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+(function(){
+
+    "use strict"
+    
+    window.onscroll = function() {myFunction()};
+    function myFunction() {
+        var navbar = document.getElementById("myNavbar");
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            navbar.className = "navbar-scroll"/*"w3-bar" + " w3-card" + " w3-animate-top" + " w3-white"*/;
+        } else {
+            navbar.className = navbar.className.replace("navbar-scroll", "navbar");
+        }
+    }
+
+})();
